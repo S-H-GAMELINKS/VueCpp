e28b704834330a653b12a6f7a1596938e89811ce
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-08 20:38:12 +0900

    using vue components

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,22 +1,31 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 module.exports = {
-    entry: './src/index.js',
-    output: { 
-      filename: 'index.js',
-      path: `${__dirname}` 
-    },
-    module: {
-      rules: [
-      {
-          test: /\.css$/,
-          use: [
-              'style-loader',
-              'css-loader'
-          ]
-      }]
+  entry: './src/index.js',
+  output: { 
+    filename: 'index.js',
+    path: `${__dirname}` 
   },
-    resolve: {
-        alias: {
-          'vue$': 'vue/dist/vue.esm.js'
-        }
+  module: {
+    rules: [
+    {
+      test: /\.vue$/,
+      use: 'vue-loader'
+    },
+    {
+        test: /\.css$/,
+        use: [
+            'style-loader',
+            'css-loader'
+        ]
+    }]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
     }
-  };
+  },
+  plugin: [
+    new VueLoaderPlugin()
+  ]
+};
